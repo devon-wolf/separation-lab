0f5797d1a67a69cd09cc4355eb93c2e4eaec0dac
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-24 14:06:27 -0700

    Add get by ID route and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -51,4 +51,18 @@ describe('03_separation-of-concerns-demo routes', () => {
       quantity: 10,
     }]);
   });
+
+  it('gets a single order by its ID', async () => {
+    await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 });
+
+    const res = await request(app)
+      .get('/api/v1/orders/1');
+
+    expect(res.body).toEqual({
+      id: '1',
+      quantity: 10,
+    });
+  });
 });
