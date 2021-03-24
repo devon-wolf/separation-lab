98b2f2d1217f772b70f45cf51e97d84211cb3298
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-24 15:14:49 -0700

    Add text message function to put and delete services

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,20 +14,7 @@ describe('03_separation-of-concerns-demo routes', () => {
     return setup(pool);
   });
 
-  it('creates a new order in our database and sends a text message', () => {
-    return request(app)
-      .post('/api/v1/orders')
-      .send({ quantity: 10 })
-      .then((res) => {
-        // expect(createMessage).toHaveBeenCalledTimes(1);
-        expect(res.body).toEqual({
-          id: '1',
-          quantity: 10,
-        });
-      });
-  });
-
-  it('ASYNC/AWAIT: creates a new order in our database and sends a text message', async () => {
+  it('creates a new order in our database', async () => {
     const res = await request(app)
       .post('/api/v1/orders')
       .send({ quantity: 10 });
