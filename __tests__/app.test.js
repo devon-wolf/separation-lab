125c61b73f4db6449c94ea936845010b79e647ff
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-24 13:47:44 -0700

    Add get all route and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -37,4 +37,18 @@ describe('03_separation-of-concerns-demo routes', () => {
       quantity: 10,
     });
   });
+
+  it('gets all orders from our database', async () => {
+    await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 });
+    
+    const res = await request(app)
+      .get('/api/v1/orders');
+
+    expect(res.body).toEqual([{
+      id: '1',
+      quantity: 10,
+    }]);
+  });
 });
