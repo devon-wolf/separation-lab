ea350bf8be0958745d96b2a1f3e4139cc5912d2c
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-24 17:28:11 -0700

    Update tests to use mock for SMS function, refactor tests to seed an order into the db before each

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,15 +3,20 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-jest.mock('twilio', () => () => ({
-  messages: {
-    create: jest.fn(),
-  },
-}));
+jest.mock('../lib/utils/twilio');
+const twilio = require('../lib/utils/twilio');
 
 describe('order routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  beforeEach(async () => {
+    await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 });
+
+      twilio.sendSms.mockClear();
   });
 
   it('creates a new order in our database', async () => {
@@ -20,24 +25,20 @@ describe('order routes', () => {
       .send({ quantity: 10 });
 
     expect(res.body).toEqual({
-      id: '1',
+      id: '2',
       quantity: 10,
     });
   });
 
-  // it('sends a text message on new order', async () => {
-  //   await request(app)
-  //     .post('/api/v1/orders')
-  //     .send({ quantity: 10 });
-
-  //   expect(/* the SMS function */).toHaveBeenCalledTimes(1);
-  // });
-
-  it('gets all orders from our database', async () => {
+  it('sends a text message on new order', async () => {
     await request(app)
       .post('/api/v1/orders')
       .send({ quantity: 10 });
-    
+
+    expect(twilio.sendSms).toHaveBeenCalledTimes(1);
+  });
+
+  it('gets all orders from our database', async () => {
     const res = await request(app)
       .get('/api/v1/orders');
 
@@ -48,10 +49,6 @@ describe('order routes', () => {
   });
 
   it('gets a single order by its ID', async () => {
-    await request(app)
-      .post('/api/v1/orders')
-      .send({ quantity: 10 });
-
     const res = await request(app)
       .get('/api/v1/orders/1');
 
@@ -62,10 +59,6 @@ describe('order routes', () => {
   });
 
   it('edits an order', async () => {
-    await request(app)
-      .post('/api/v1/orders')
-      .send({ quantity: 10 });
-
     const res = await request(app)
       .put('/api/v1/orders/1')
       .send({ quantity: 5 });
@@ -76,19 +69,15 @@ describe('order routes', () => {
       });
   });
 
-  // it('sends a text message on order update', async () => {
-  //   await request(app)
-  //     .put('/api/v1/orders/1')
-  //     .send({ quantity: 5 });
+  it('sends a text message on order update', async () => {
+    await request(app)
+      .put('/api/v1/orders/1')
+      .send({ quantity: 5 });
 
-  //   expect(/* the SMS function */).toHaveBeenCalledTimes(1);
-  // });
+    expect(twilio.sendSms).toHaveBeenCalledTimes(1);
+  });
 
   it('deletes an order', async () => {
-    await request(app)
-      .post('/api/v1/orders')
-      .send({ quantity: 10 });
-
     const res = await request(app)
       .delete('/api/v1/orders/1');
 
@@ -98,10 +87,10 @@ describe('order routes', () => {
     });
   });
 
-  // it('sends a text message on order delete', async () => {
-  //   await request(app)
-  //     .delete('/api/v1/orders/1');
+  it('sends a text message on order delete', async () => {
+    await request(app)
+      .delete('/api/v1/orders/1');
 
-  //   expect(/* the SMS function */).toHaveBeenCalledTimes(1);
-  // });
+      expect(twilio.sendSms).toHaveBeenCalledTimes(1);
+  });
 });
